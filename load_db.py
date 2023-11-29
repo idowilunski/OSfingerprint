4c8246df011dad32c05d95c39359d0bca0d8f634
idowilunski <idowilunski@gmail.com>
2023-11-29 17:35:43 +0200

    finished DB parser

--- load_db.py
+++ load_db.py
@@ -26,54 +26,57 @@ class DatabaseParser:
         lines = entry.split('\n')
         entry_data = {}
         for line in lines:
-            print(line + '\n')
             # skip irrelevant lines
-            if line[0] == '#' or line.strip().startswith('Class'):
+            if not line or line[0] == '#' or line.strip().startswith('Class'):
                 continue
 
             # CPE line needs different handling
             if line.strip().startswith('CPE') or line.strip().startswith('Fingerprint'):
                 key, value = line.split(' ', 1)
                 entry_data[key] = value.strip()
-                print(entry_data)
 
             # all other lines
             else:
                 key, value = line.split('(', 1)
                 entry_data[key] = value.rstrip(')')
+        print(entry_data)
         return entry_data
 
     def read_database(self):
-        with open(self.db_path, 'r') as file:
+        with open(self.db_path, 'r', encoding='utf-8', errors='ignore') as file:
+            count = 0
             entries = file.read().split('\n\n')
             for entry in entries:
-                if entry:
-                    entry_data = self.parse_entry(entry)
-                    fingerprint = Fingerprint(
-                        entry_data.get('Fingerprint', ''),
-                        entry_data.get('CPE', ''),
-                        entry_data.get('SEQ', ''),
-                        entry_data.get('OPS', ''),
-                        entry_data.get('WIN', ''),
-                        entry_data.get('ECN', ''),
-                        entry_data.get('T1', ''),
-                        entry_data.get('T2', ''),
-                        entry_data.get('T3', ''),
-                        entry_data.get('T4', ''),
-                        entry_data.get('T5', ''),
-                        entry_data.get('T6', ''),
-                        entry_data.get('T7', ''),
-                        entry_data.get('U1', ''),
-                        entry_data.get('IE', '')
+                if count < 2:
+                    count += 1
+                    continue
+
+                entry_data = self.parse_entry(entry)
+                fingerprint = Fingerprint(
+                    entry_data.get('Fingerprint', ''),
+                    entry_data.get('CPE', ''),
+                    entry_data.get('SEQ', ''),
+                    entry_data.get('OPS', ''),
+                    entry_data.get('WIN', ''),
+                    entry_data.get('ECN', ''),
+                    entry_data.get('T1', ''),
+                    entry_data.get('T2', ''),
+                    entry_data.get('T3', ''),
+                    entry_data.get('T4', ''),
+                    entry_data.get('T5', ''),
+                    entry_data.get('T6', ''),
+                    entry_data.get('T7', ''),
+                    entry_data.get('U1', ''),
+                    entry_data.get('IE', '')
                     )
-                    self.fingerprints.append(fingerprint)
+                self.fingerprints.append(fingerprint)
 
     def get_fingerprints(self):
         return self.fingerprints
 
 
 def main():
-    db_path = 'C:\\Users\\idowi\\Desktop\\OSfingerprint\\DB_example.txt'
+    db_path = 'C:\\Users\\idowi\\Desktop\\OSfingerprint\\nmap-os-db.txt'
     parser = DatabaseParser(db_path)
     parser.read_database()
     fingerprints = parser.get_fingerprints()
